18c1cd99927d07d2b26b32c4c70b20d25efe97a3
Alex-Andersson <alex199andersson@gmail.com>
2023-03-07 09:26:56 +0100

    initial commit

---
+++ src/pages/home.page.tsx
@@ -0,0 +1,15 @@
+const HomePage = () => {
+  return (
+    <>
+      <section className="bg-[#a33474] min-h-screen pt-20">
+        <div className="max-w-4xl mx-auto bg-ct-dark-100 rounded-md h-[20rem] flex justify-center items-center backdrop-opacity-10 backdrop-invert bg-white/30">
+          <p className="text-5xl font-semibold">
+            Welcome to Gaddr
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default HomePage;
---
+++ src/utils/getTikTokUrl.ts
@@ -0,0 +1,15 @@
+export const getTikTokUrl = (from: string) => {
+    const rootUrl = `https://www.tiktok.com/auth/authorize/`;
+  
+    const options = {
+      redirect_uri: import.meta.env.VITE_TIKTOK_OAUTH_REDIRECT as string,
+      client_Key: import.meta.env.VITE_TIKTOK_OAUTH_CLIENT_KEY as string,
+      response_type: "code",
+      scope: "user.info.basic,user.info.email,user.info.phone_number",
+      state: from,
+    };
+  
+    const qs = new URLSearchParams(options);
+  
+    return `${rootUrl}?${qs.toString()}`;
+  };
